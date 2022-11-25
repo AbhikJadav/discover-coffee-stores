d896e3aa6e757b112eb77f562c0dfd406d7611db
AbhikJadav <abhik.jadav002@gmail.com>
2022-11-25 18:43:37 +0530

    setup next-App

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Head from "next/head";
 
-const Index = () => {
-  return <div>Coffee store page</div>;
+const CoffeeStore = () => {
+  const router = useRouter();
+  console.log("router:", router.query.id);
+  return (
+    <div>
+      <Head>
+        <title>{router.query.id}</title>
+      </Head>
+      Coffee store page {router.query.id}
+      <Link href="/">Back To Home Page.</Link>
+      <Link href="/coffee-store/dynamic">Go to dynamic</Link>
+    </div>
+  );
 };
 
-export default Index;
+export default CoffeeStore;
